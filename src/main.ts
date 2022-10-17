27b423c85c02d6153cdb3504405d93dfc88367b0
Clem Borja <clem.borj30@gmail.com>
2022-10-17 08:18:13 +0800

    Nav with user auth state & firebase app init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './app/app.module'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let appInit = false
+firebase.initializeApp(environment.firebase)
+firebase.auth().onAuthStateChanged(() => {
+  if (!appInit) {
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err))
+  }
+  appInit = true
+})
