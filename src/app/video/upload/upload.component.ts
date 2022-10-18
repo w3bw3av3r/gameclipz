98aa2105b60a75219e598a6915a21b6220c63d9c
Clem Borja <clem.borj30@gmail.com>
2022-10-18 11:21:36 +0800

    File handling & validation & multistep form visibility

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core'
 })
 export class UploadComponent implements OnInit {
   isDragOver = false
+  file: File | null = null
+  nextStep = false
+
   constructor() {}
 
   ngOnInit(): void {}
 
   storeFile(event: Event) {
     this.isDragOver = false
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+
+    this.nextStep = true
   }
 }
