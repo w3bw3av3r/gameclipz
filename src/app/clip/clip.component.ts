7dee91255152c25ccbda4f5001a71ec15b12f9f9
Clem Borja <clem.borj30@gmail.com>
2022-10-20 13:57:43 +0800

    Setup video player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core'
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
+import videojs from 'video.js'
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClipComponent implements OnInit {
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef
+
   id = ''
+  player?: videojs.Player
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement)
+
     this.route.params.subscribe((params: Params) => {
       this.id = params.id
     })
