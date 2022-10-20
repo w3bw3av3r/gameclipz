eac5eabe9bb1da8e2d07496b59343fa2312eac77
Clem Borja <clem.borj30@gmail.com>
2022-10-20 13:19:34 +0800

    Display clips list in home and clip page

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -8,25 +8,27 @@ import {
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFireStorage } from '@angular/fire/compat/storage'
 import IClip from '../models/clip.model'
-import { BehaviorSubject, of, combineLatest } from 'rxjs'
+import { BehaviorSubject, of, combineLatest, lastValueFrom } from 'rxjs'
 import { switchMap, map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClipService {
-  public clipCollection: AngularFirestoreCollection<IClip>
+  public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClipz: IClip[] = []
+  pendingRequest = false
 
   constructor(
     private db: AngularFirestore,
     private auth: AngularFireAuth,
     private storage: AngularFireStorage
   ) {
-    this.clipCollection = db.collection('clipz')
+    this.clipsCollection = db.collection('clipz')
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
-    return this.clipCollection.add(data)
+    return this.clipsCollection.add(data)
   }
 
   getUserClipz(sort$: BehaviorSubject<string>) {
@@ -36,7 +38,7 @@ export class ClipService {
         if (!user) {
           return of([])
         }
-        const query = this.clipCollection.ref
+        const query = this.clipsCollection.ref
           .where('uid', '==', user.uid)
           .orderBy('timestamp', sort === '1' ? 'desc' : 'asc')
         return query.get()
@@ -46,7 +48,7 @@ export class ClipService {
   }
 
   updateClip(id: string, title: string) {
-    return this.clipCollection.doc(id).update({
+    return this.clipsCollection.doc(id).update({
       title,
     })
   }
@@ -59,6 +61,35 @@ export class ClipService {
     clipRef.delete()
     screenshotRef.delete()
 
-    await this.clipCollection.doc(clip.docID).delete()
+    await this.clipsCollection.doc(clip.docID).delete()
+  }
+
+  async getClipz() {
+    if (this.pendingRequest) {
+      return
+    }
+    this.pendingRequest = true
+    let query = this.clipsCollection.ref.orderBy('timestamp', 'desc').limit(6)
+
+    const { length } = this.pageClipz
+
+    if (length) {
+      const lastDocID = this.pageClipz[length - 1].docID
+      const lastDoc = await lastValueFrom(
+        this.clipsCollection.doc(lastDocID).get()
+      )
+
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+    snapshot.forEach((doc) => {
+      this.pageClipz.push({
+        docID: doc.id,
+        ...doc.data(),
+      })
+    })
+
+    this.pendingRequest = false
   }
 }
