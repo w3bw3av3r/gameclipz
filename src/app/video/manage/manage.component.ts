04477fe9d7dc8644480707365f23643370524ff6
Clem Borja <clem.borj30@gmail.com>
2022-10-20 14:52:23 +0800

    Copy link feature in manage page

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -71,4 +71,16 @@ export class ManageComponent implements OnInit {
     this.activeClip = clip
     this.modal.toggleModal('editClip')
   }
+
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault()
+    if (!docID) {
+      return
+    }
+
+    const url = `${location.origin}/clip/${docID}`
+    await navigator.clipboard.writeText(url)
+
+    alert('Link copied!')
+  }
 }
